02ead2c6b49f6ac533fdb2f98894c5cd56eadc8a
loganocean <43765068+loganocean@users.noreply.github.com>
2018-12-20 01:06:17 -0500

    Add files via upload

---
+++ lesson8/problem1.py
@@ -0,0 +1,5 @@
+age = input('What is your age today')
+age = int(age)
+born = 2018 - age 
+answer = born + 79
+print(answer)
---
+++ lesson8/problem2.py
@@ -0,0 +1,32 @@
+message = 'tjwz wz tjq zqurqt pqzzagq'
+
+# do not change the code above this line
+
+# Write your algorithm to decode the message here:
+
+message = 'tjwz wz tjq zqurqt pqzzagq'
+message.replace('q' , 'e')
+print(message)
+message = 'tjwz wz tjq zqurqt pqzzagq'
+message.replace('z' , 's')
+print(message)
+message = 'tjwz wz tjq zqurqt pqzzagq'
+message.replace('p' , 'm')
+print(message)
+message = 'tjwz wz tjq zqurqt pqzzagq'
+message.replace('u' , 'c')
+print(message)
+message = 'tjwz wz tjq zqurqt pqzzagq'
+message.replace('w' , 'i')
+print(message)
+message = 'tjwz wz tjq zqurqt pqzzagq'
+message.replace('j' , 'h')
+print(message)
+
+
+
+
+
+# do not change the code below this line
+
+print(message)
